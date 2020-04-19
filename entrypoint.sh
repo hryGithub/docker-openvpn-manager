563b827371cb1ffd391df287d81bbdaf01d475c5
hujian <516870171@qq.com>
2020-04-19 16:36:06 +0800

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,24 +12,41 @@ sed -i "s@DB='openvpn-admin'@DB='$DB_NAME'@g" /etc/openvpn/scripts/config.sh    
 sed -i "s@proto tcp@proto $OVPN_PROTO@g" /etc/openvpn/server.conf
 sed -i "s@port 1194@port $OVPN_PORT@g" /etc/openvpn/server.conf
 
+init-pki(){
+    cd $OPENVPN 
+    easyrsa init-pki
+    easyrsa build-ca nopass
+    easyrsa gen-dh
+    easyrsa build-server-full server nopass
+    openvpn --genkey --secret $EASYRSA/pki/ta.key
+    cp $EASYRSA/pki/{ca.crt,ta.key,issued/server.crt,private/server.key,dh.pem} "/etc/openvpn/"
+}
+
+#init-pki
+if [ ! -f '/etc/openvpn/ca.crt'];then
+    init_pki()
+fi
 
 #config ovpn-client
 if [ $OVPN_PROTO = udp ];then
     sed -i "s@port 1194@port $OVPN_PORT@g" /etc/openvpn/server.conf
 fi
-for file in $(find -name client.ovpn); do
-    sed -i "s/remote xxx\.xxx\.xxx\.xxx 443/remote $OVPN_ADDR $OVPN_PORT/" $file
-    echo "<ca>" >> $file
-    cat "/etc/openvpn/ca.crt" >> $file
-    echo "</ca>" >> $file
-    echo "<tls-auth>" >> $file
-    cat "/etc/openvpn/ta.key" >> $file
-    echo "</tls-auth>" >> $file
+if [ ! -d /var/www/localhost/htdocs/client-conf ];then
+    cp -r /var/www/localhost/htdocs/installation/client-conf /var/www/localhost/htdocs/
+    for file in $(find /var/www/localhost/htdocs/client-conf -name client.ovpn); do
+        sed -i "s/remote xxx\.xxx\.xxx\.xxx 443/remote $OVPN_ADDR $OVPN_PORT/" $file
+        echo "<ca>" >> $file
+        cat "/etc/openvpn/ca.crt" >> $file
+        echo "</ca>" >> $file
+        echo "<tls-auth>" >> $file
+        cat "/etc/openvpn/ta.key" >> $file
+        echo "</tls-auth>" >> $file
 
-  if [ $openvpn_proto = "udp" ]; then
-    sed -i "s/proto tcp-client/proto udp/" $file
-  fi
-done
+    if [ $openvpn_proto = "udp" ]; then
+        sed -i "s/proto tcp-client/proto udp/" $file
+    fi
+    done
+fi
 #iptables init
 mkdir -p /dev/net
 if [ ! -c /dev/net/tun ]; then
@@ -42,22 +59,6 @@ genconfig(){
     echo 1
 }
 
-init-pki(){
-    cd $OPENVPN 
-    easyrsa init-pki
-    easyrsa build-ca nopass
-    easyrsa gen-dh
-    easyrsa build-server-full server nopass
-    openvpn --genkey --secret $EASYRSA/pki/ta.key
-    cp $EASYRSA/pki/{ca.crt,ta.key,issued/server.crt,private/server.key,dh.pem} "/etc/openvpn/"
-}
-
-
-
-#init-pki
-if [ ! -f '/etc/openvpn/ca.crt'];then
-    init_pki()
-fi
 
 httpd -DFOREGROUND
 #/usr/sbin/openvpn --config /etc/openvpn/server.conf
