792f69b448569bc97947f158cc6cad610a1d6141
hujian <hujian@tigerft.com>
2020-05-15 11:49:39 +0800

    修复bug

--- openvpn-manager/index.php
+++ openvpn-manager/index.php
@@ -195,7 +195,7 @@
             </div>
             <div class="col-md-6">
               <a class="navbar-text navbar-right" href="index.php?logout" title="Logout"><button class="btn btn-danger">Logout <span class="glyphicon glyphicon-off" aria-hidden="true"></span></button></a>
-              <a class="navbar-text navbar-right" href="index.php" title="Configuration"><button class="btn btn-default">Configrations</button></a>
+              <a class="navbar-text navbar-right" href="index.php" title="Configuration"><button class="btn btn-default">Configurations</button></a>
             </p>
           </div>
         </div>
